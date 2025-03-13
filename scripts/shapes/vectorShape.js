1704f96df8695bb34f47c7d45c1d41d692bd6e35
duquevieira <joaopaulo@skillsworkflow.com>
2025-03-13 21:40:28 +0000

    Added trajectory to vector.

--- scripts/shapes/vectorShape.js
+++ scripts/shapes/vectorShape.js
@@ -13,20 +13,48 @@ class VectorShape extends BaseShape {
         var items = instance.option("items");
         items.push(
             {
+                colSpan: 1,
                 dataField: "xTerm",
-                label: { text: "Horizontal Variable" },
+                label: { text: "Horizontal" },
                 editorType: "dxTextBox",
                 editorOptions: {
                     stylingMode: "filled"
                 }
             },
             {
+                colSpan: 1,
                 dataField: "yTerm",
-                label: { text: "Vertical Variable" },
+                label: { text: "Vertical" },
                 editorType: "dxTextBox",
                 editorOptions: {
                     stylingMode: "filled"
                 }
+            },
+            {
+                colSpan: 2,
+                dataField: "trajectoryColor",
+                label: { text: "Trajectory color" },
+                editorType: "dxButtonGroup",
+                editorOptions: {
+                    onContentReady: function(e) {
+                        e.component.option("items").forEach((item, index) => {
+                            const buttonElement = e.element.find(`.dx-button:eq(${index})`);
+                            buttonElement.find(".dx-icon").css("color", item.color == "#00000000" ? "#cccccc" : item.color);
+                        });
+                    },
+                    items: this.board.theme.getBackgroundColors().map(c => ({
+                        icon: "fa-solid " + (c.color == "#00000000" ? "fa-square-dashed" : "fa-square"),
+                        color: c.color
+                    })),
+                    keyExpr: "color",
+                    stylingMode: "text",
+                    selectedItemKeys: [this.properties.trajectoryColor],
+                    onItemClick: e => {
+                        let formInstance = $("#shape-form").dxForm("instance");
+                        formInstance.updateData("trajectoryColor", e.itemData.color);
+                        this.setProperty("trajectoryColor", e.itemData.color);
+                    }
+              }
             }
         );
         instance.option("items", items);
@@ -39,19 +67,32 @@ class VectorShape extends BaseShape {
         this.properties.y = 0;
         this.properties.width = 30;
         this.properties.height = 30;
-        this.properties.foregroundColor = this.board.theme.getStrokeColors()[0].color;
+        this.properties.foregroundColor = this.board.theme.getStrokeColors()[1].color;
         this.properties.backgroundColor = this.board.theme.getBackgroundColors()[1].color;
+        this.properties.trajectoryColor = this.board.theme.getBackgroundColors()[0].color;
     }
 
     createElement() {
-        const path = this.board.createSvgElement("path");
-        return path;
+        const element = this.board.createSvgElement("g");
+        this.path = this.board.createSvgElement("path");
+        element.appendChild(this.path);
+        this.trajectory = { element: this.board.createSvgElement("polyline"), values: [] };
+        this.trajectory.element.setAttribute("fill", "none");
+        element.appendChild(this.trajectory.element);
+        return element;
     }    
 
     update() {
         super.update();
-        this.properties.width = this.properties.xTerm != "" ? this.board.calculator.getByName(this.properties.xTerm) : this.properties.width;
-        this.properties.height = this.properties.yTerm != "" ? this.board.calculator.getByName(this.properties.yTerm) : this.properties.height; 
+        const calculator = this.board.calculator;
+        this.properties.width = calculator.getByName(this.properties.xTerm) ?? this.properties.width;
+        this.properties.height = calculator.getByName(this.properties.yTerm) ?? this.properties.height; 
+        var currentIteration = this.trajectory.values.length;
+        this.trajectory.values = this.trajectory.values.slice(0, calculator.getLastIteration());
+        if (this.trajectory.values.length <= calculator.getLastIteration()) {
+            const position = this.getBoardPosition();
+            this.trajectory.values.push({ x: position.x + this.properties.width, y: position.y + this.properties.height });
+        }
     }
 
     draw() {
@@ -73,9 +114,16 @@ class VectorShape extends BaseShape {
             M ${startX} ${startY} L ${tipX} ${tipY}
             L ${leftX} ${leftY} L ${rightX} ${rightY} L ${tipX} ${tipY} Z
         `;
-        this.element.setAttribute("d", arrowPath);
-        this.element.setAttribute("fill", this.properties.backgroundColor);
-        this.element.setAttribute("stroke", this.properties.foregroundColor);
-        this.element.setAttribute("stroke-width", 1);
+        this.path.setAttribute("d", arrowPath);
+        this.path.setAttribute("fill", this.properties.backgroundColor);
+        this.path.setAttribute("stroke", this.properties.foregroundColor);
+        this.path.setAttribute("stroke-width", 1);
+        if (this.properties.trajectoryColor != this.board.theme.getBackgroundColors()[0].color) {
+            const trajectoryPolyLine = this.trajectory.values.map(v => `${v.x},${v.y}`).join(" ");
+            this.trajectory.element.setAttribute("points", trajectoryPolyLine);
+            this.trajectory.element.setAttribute("stroke", this.properties.trajectoryColor);
+            this.trajectory.element.setAttribute("stroke-width", 1);
+        } else
+            this.trajectory.element.removeAttribute("points");
     }
 }
